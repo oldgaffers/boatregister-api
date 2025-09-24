64473e69361f696c173ec21558665d4bf4961ab9
jcable <boatregister@oga.org.uk>
2025-09-24 16:46:41 +0100

    fix: improve api use

--- boatregister_api/geonames.py
+++ boatregister_api/geonames.py
@@ -2,17 +2,8 @@ import json
 import urllib.request
 import urllib.parse
 
-def choose(names, name):
-  places = [gn for gn in names if gn['fcl'] == 'P']
-  if len(places) > 0:
-    exact = [gn for gn in places if gn['name'] == name]
-    if len(exact) > 0:
-      return exact[0]
-    return places[0]
-  return names[0]
-
 def geonames(place):
-  url = f'https://secure.geonames.org/searchJSON?username=oga_boatregister&country=uk&country=ie&name={urllib.parse.quote(place)}'
+  url = f'https://secure.geonames.org/searchJSON?username=oga_boatregister&featureCode=p&country=uk&country=ie&name_equals={urllib.parse.quote(place)}'
   # print(url)
   f = urllib.request.urlopen(url)
   p = json.loads(f.read())
@@ -20,7 +11,8 @@ def geonames(place):
   if n == 1:
     data = p['geonames'][0]
   if n > 1:
-    data = choose(p['geonames'], place)
+    sorted_names = sorted(p['geonames'], key=lambda k: k['population'], reverse=True)
+    data = sorted_names[0]
   if n == 0:
     data = { 'message': 'not found' }
   return data
