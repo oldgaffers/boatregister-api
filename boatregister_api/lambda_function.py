14b689cf0464a635b515f9f0e40f7fa3883d624d
jcable <boatregister@oga.org.uk>
2025-10-02 07:29:56 +0100

    fix: method

--- boatregister_api/lambda_function.py
+++ boatregister_api/lambda_function.py
@@ -196,8 +196,9 @@ def lambda_handler(event, context):
     print(json.dumps(event))
     scope = event['pathParameters'].get('scope', 'public')
     table = event['pathParameters'].get('table', '')
-    if 'authorizer' in event['requestContext']:
-        auth = event['requestContext']['authorizer']
+    rq = event['requestContext']
+    if 'authorizer' in rq:
+        auth = rq['authorizer']
         if 'jwt' in auth:
             claims = auth['jwt']['claims']
         else:
@@ -214,11 +215,12 @@ def lambda_handler(event, context):
             'statusCode': 403,
             'body': json.dumps("user does not have permission to access this table")
         } 
-    if event['httpMethod'] == 'GET':
-        return gets(scope, table, event['queryStringParameters'], event['requestContext']['requestTimeEpoch'])
-    elif event['httpMethod'] == 'PUT':
+    method = event.get('httpMethod', rq['http']['Method'])
+    if method == 'GET':
+        return gets(scope, table, event['queryStringParameters'], rq['requestTimeEpoch'])
+    elif method == 'PUT':
         return puts(scope, table, json.loads(event['body']))
-    elif event['httpMethod'] == 'POST':
+    elif method == 'POST':
         return posts(scope, table, json.loads(event['body']))
     else:
         return {
