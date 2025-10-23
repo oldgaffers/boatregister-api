2f7bde885cc1b14816ab3be234bbccb60ccd5419
Julian Cable <julian.cable@yahoo.com>
2025-10-23 22:56:55 +0100

    Update tables.py

--- boatregister_api/tables.py
+++ boatregister_api/tables.py
@@ -19,7 +19,7 @@ def gold():
 
 def getMember(member):
     g = gold()
-    n = [m for m in g if m['ID'] == member and m['Status'] != 'Left OGA']
+    n = [m for m in g if m['ID'] == member and m['Status'] not in ['Deceased', 'Left OGA']]
     if len(n) > 0:
         return n[0]
     return None
