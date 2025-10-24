ba6728241c5b1f616df0fa7d02fbe1a39bf0023c
Julian Cable <julian.cable@yahoo.com>
2025-10-24 04:48:36 +0100

    Update tables.py

--- boatregister_api/tables.py
+++ boatregister_api/tables.py
@@ -1,67 +1,13 @@
 from decimal import Decimal
 from updates import update_tables
 from places import geocode
+from emails import contact, profile, compose_enquiry
 from mail import sendmail
 from summarise import buildersummary
 import boto3
 import simplejson as json
 
-s3 = boto3.client('s3')
 dynamodb = boto3.resource('dynamodb')
-
-def json_from_object(bucket, key):
-    r = s3.get_object(Bucket=bucket, Key=key)
-    text = r["Body"].read().decode('utf-8')
-    return json.loads(text)
-    
-def gold():
-    return json_from_object('boatregister', 'gold/latest.json')
-
-def getMember(member):
-    g = gold()
-    n = [m for m in g if m['ID'] == member and m['Status'] not in ['Deceased', 'Left OGA']]
-    if len(n) > 0:
-        return n[0]
-    return None
-
-def getDear(members):
-    n = [o['Firstname'] for o in members]
-    if len(n) > 0:
-        return ' and '.join(n)
-    return "folks"
-
-def compose_contact(body):
-    id = body.get('id', body.get('member', None))
-    # print('compose_contact', id)
-    if id is None:
-        return None
-    member = getMember(id)
-    dear = getDear([member])
-    your = 'your '
-    name = 'an OGA member'
-    if 'name' in body and len(body['name'].strip()) > 0:
-        name = body['name']
-    text = [
-        f"Dear {dear},",
-        f"{name} would like to contact you regarding:",
-        f"{body['text']}."
-    ]
-    text.append(f"They can be contacted at {body['email']}.")
-    text.append("If our records are out of date and this email is not appropriate, please accept our apologies.")
-    text.append("You can contact us by replying to this email or via our website oga.org.uk.")
-    mail = { 'subject': 'hello from an OGA member' }
-    mail['message'] = "\n".join(text)
-    mail['to'] = [member['Email']]
-    # print('compose_contact', mail)
-    return mail
-
-def compose_enquiry(body):
-    mail = { 'subject': body['topic']}
-    text = [f"{body['name']} has expressed interest in {body['topic']}.", 'The details are:']
-    mail['message'] = "\n".join(text + [f"{field}: {body[field]}" for field in body.keys()])
-    mail['to'] = [ body['email'], f"{body['topic']}@oga.org.uk"]
-    # print('compose_enquiry', mail)
-    return mail
 
 def paramMap(val):
     if val == 'true':
@@ -73,9 +19,6 @@ def paramMap(val):
     except:
         return val
 
-def contact(body):
-    return sendmail(compose_contact(body))
-    
 def enquiry(body):
     sns = boto3.client('sns')
     sns.publish(
@@ -88,17 +31,6 @@ def enquiry(body):
         'statusCode': 200,
         'body': json.dumps("Ok")
     }
-
-def compose_profile(body):
-    id = body.get('id', body.get('member', None))
-    mail = { 'subject': f"Membership data change request for {body['firstname']} {body['lastname']} ({id})"}
-    mail['message'] = body['text']
-    mail['to'] = ["membership@oga.org.uk"]
-    return mail
-
-def profile(body):
-    mail = compose_profile(body)
-    return sendmail(mail)
 
 def map_values(d):
     r = {}
